223f45b78e40378bf51c22527f6b1efb4e65b674
Alfonse12 <mudosamugisho@gmail.com>
2019-04-29 23:26:39 +0930

    Deliverable2 in progress

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+document.getElementById('button1').addEventListener('click',function(){
+  document.querySelector('.bg-modal').style.display = 'flex';
+});
+
+document.querySelector('.close').addEventListener('click',function(){
+  document.querySelector('.bg-modal').style.display = 'none';
+});
